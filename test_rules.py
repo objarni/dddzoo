298c24fbc6fc8075b356ceb64a4610f3330341d4
Olof Bjarnason <olof.bjarnason@gmail.com>
2014-06-19 22:20:26 +0100

    added ignore file and rules outline

---
+++ test_rules.py
@@ -0,0 +1,26 @@
+# coding: utf-8
+# DDD Zoo
+'''
+Rule 1. Carnivors and herbivors cannot live together.
+For example, moving a lion to an enclosement with a zebra is not good, since the zebra will be eaten.
+
+Rule 2: Herbivors can live side-by-side.
+Moving a giraffe to the enclosement of an elephant is OK, since both species are herbivors and wont kill each other.
+
+Rule 3: Carnivors of differing species cannot live together.
+Moving a tiger to the enclosement of a lion is not good. Only predators of the same species can safely reside in the same enclosement.
+
+Rule 4: Multiple carnivors of same species can live together.
+Moving a lion to another lions enclosement is OK.
+
+Rule 5: Flying animals needs to be in cages.
+Moving a bird from a cage to a fenced enclosement is not OK, since the bird will simply fly away.
+
+Rule 6: Strong animals needs to be in walled enclosements.
+Moving a rhino from a walled enclosement to a fenced enclosement is not OK, since it will just run through the fences.
+
+Rule 7: Midsize animals can live in both walled and fenced enclosements.
+Moving a zebra from a walled enclosement to a fenced enclosement is OK, since it won’t run down the fences.
+'''
+
+print("hello world!")
